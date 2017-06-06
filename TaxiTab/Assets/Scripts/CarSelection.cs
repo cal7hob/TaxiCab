bc0922f21a893ffd01e6af459032ae038c9a0143
AJ-1000\Anjum <Anjumshakeel1234@gmail.com>
2017-06-06 23:40:35 +0500

    1. Exported both UI packages within the project 2. Attached some missing colliders 3. Attached Missing Bridges 4. Occlusion Culling Updated 5. Tested All Citiies 6. Removed some objects for better gameplay
    Test Needed from both of you. And Usman mjhe Ahmed wala project dena hai tune.

---
+++ TaxiTab/Assets/Scripts/CarSelection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TinHead_Developer{
+public class CarSelection : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject[] Cars;
+	int movement=0;
+    public Camera Cam;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			Cam.gameObject.transform.position= Vector3.MoveTowards (Cam.transform.position, Cars [movement].transform.position,Time.deltaTime*20f);
+	}
+	public void LoadLevel(){
+			GameManager.Instance.SelectedCar = movement;
+		GameManager.Instance.Play(3);
+
+	}
+		public void NextCar(){
+			
+			if (movement < Cars.Length-1) {
+				movement++;
+			}
+
+		}
+		public void PreviousCar(){
+			if (movement >0) {
+				movement--;
+			}
+		}
+
+     }
+}
