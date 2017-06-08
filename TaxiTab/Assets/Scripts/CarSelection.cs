fb8482b6dfa680bad2b56f78697ebb1e862cd411
AJ-1000\Anjum <Anjumshakeel1234@gmail.com>
2017-06-08 15:09:37 +0500

    1. 2 Extra Cars included in the gameplay 2. UI Changed a little bit

--- TaxiTab/Assets/Scripts/CarSelection.cs
+++ TaxiTab/Assets/Scripts/CarSelection.cs
@@ -2,38 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TinHead_Developer{
-public class CarSelection : MonoBehaviour {
+namespace TinHead_Developer
+{
+    public class CarSelection : MonoBehaviour
+    {
 
-	// Use this for initialization
-    public GameObject[] Cars;
-	int movement=0;
-    public Camera Cam;
-	void Start () {
+        // Use this for initialization
+        public GameObject[] Cars;
+        int movement = 0;
+        public Camera Cam;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-			Cam.gameObject.transform.position= Vector3.MoveTowards (Cam.transform.position, Cars [movement].transform.position,Time.deltaTime*20f);
-	}
-	public void LoadLevel(){
-			GameManager.Instance.SelectedCar = movement;
-		GameManager.Instance.Play(3);
+        void Start()
+        {
 
-	}
-		public void NextCar(){
-			
-			if (movement < Cars.Length-1) {
-				movement++;
-			}
+        }
 
-		}
-		public void PreviousCar(){
-			if (movement >0) {
-				movement--;
-			}
-		}
+        // Update is called once per frame
+        void Update()
+        {
+            Cam.gameObject.transform.position = Vector3.MoveTowards(Cam.transform.position, Cars[movement].transform.position, Time.deltaTime * 20f);
+        }
 
-     }
+        public void LoadLevel()
+        {
+            GameManager.Instance.SelectedCar = movement;
+            GameManager.Instance.Play(3);
+        }
+
+        //Activates iTS Manager and iTS Spawner at Runtime
+        public void LoadTraffic()
+        {
+            foreach (var item in GameObject.FindGameObjectsWithTag("Traffic"))
+            {
+                item.SetActive(true);
+                Debug.Log(item.name + "   " + item.activeInHierarchy + "    " + item.activeSelf);
+            }
+        }
+        public void NextCar()
+        {
+
+            if (movement < Cars.Length - 1)
+            {
+                movement++;
+            }
+
+        }
+        public void PreviousCar()
+        {
+            if (movement > 0)
+            {
+                movement--;
+            }
+        }
+    }
 }
